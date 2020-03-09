d5c5db6defe1eac87289c23b6f35cf0a07b6643a
pplenty <newtype26@naver.com>
2020-03-09 22:40:55 +0900

    study: init commit

---
+++ src/main/java/com/pplenty/studytoby/StudyTobyApplication.java
@@ -0,0 +1,13 @@
+package com.pplenty.studytoby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTobyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyTobyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pplenty/studytoby/StudyTobyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pplenty.studytoby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTobyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
