0af22e7c73d3f77781319b217f33fcc34dcb1a6f
김다혜 <dahye432111@gmail.com>
2020-03-10 21:00:56 +0900

    1장: 싱글톤 레지스트리와 오브젝트 스코프 - 애플리케이션 컨텍스트 vs 오브젝트 팩토리
    애플리케이션과 오브젝트 팩토리는 중요한 차이점이 있다.
    애플리케이션은 매번 같은 oject를 return 해주고,
    오브젝트 팩토리는 다른 object를 return 해 준다.
    
    즉, 스프링은 여러 번에 걸쳐 빈을 요청하더라도 매번 동일한 오브젝트를 돌려준다.

---
+++ src/main/java/com/toby/tobyspring/user/ObjectTest.java
@@ -0,0 +1,25 @@
+package com.toby.tobyspring.user;
+
+import com.toby.tobyspring.user.dao.DaoFactory;
+import com.toby.tobyspring.user.dao.UserDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ObjectTest {
+    public static void main(String[] args) {
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao dao1 = daoFactory.userDao();
+        UserDao dao2 = daoFactory.userDao();
+
+        System.out.println(dao1);
+        System.out.println(dao2);
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao3 = context.getBean("userDao", UserDao.class);
+        UserDao dao4 = context.getBean("userDao", UserDao.class);
+
+        System.out.println(dao3);
+        System.out.println(dao4);
+
+    }
+}
