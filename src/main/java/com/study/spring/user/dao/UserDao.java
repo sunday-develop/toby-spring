7e553ef201bec5e1ca198644eaa6afa3fa77607d
최홍희 <vvshinevv@tmon.co.kr>
2020-03-14 02:10:36 +0900

    [2.3.3]포괄적인 테스트

--- src/main/java/com/study/spring/user/dao/UserDao.java
+++ src/main/java/com/study/spring/user/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.study.spring.user.dao;
 
 import com.study.spring.user.domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -37,16 +38,22 @@ public class UserDao {
         ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
-        rs.next();
 
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
+        User user = null;
+        if (rs.next()) {
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
 
         rs.close();
         ps.close();
         c.close();
+
+        if (user == null) {
+            throw new EmptyResultDataAccessException(1);
+        }
 
         return user;
     }
--- src/test/java/com/study/spring/user/dao/UserDaoTest.java
+++ src/test/java/com/study/spring/user/dao/UserDaoTest.java
@@ -1,15 +1,16 @@
 package com.study.spring.user.dao;
 
 import com.study.spring.user.domain.User;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserDaoTest {
 
@@ -20,20 +21,36 @@ public class UserDaoTest {
         ApplicationContext context = new GenericXmlApplicationContext("spring/applicationContext.xml");
         UserDao userDao = context.getBean("userDao", UserDao.class);
 
+        User user1 = new User("user1", "username1", "username11");
+        User user2 = new User("user2", "username2", "username22");
+
         userDao.deleteAll();
         assertEquals(userDao.getCount(), 0);
 
-        User user1 = new User();
-        user1.setId("whiteship");
-        user1.setName("back");
-        user1.setPassword("married");
-
         userDao.add(user1);
-        assertEquals(userDao.getCount(), 1);
+        userDao.add(user2);
+        assertEquals(userDao.getCount(), 2);
 
-        User user2 = userDao.get(user1.getId());
+        User userTarget1 = userDao.get(user1.getId());
+        assertEquals(userTarget1.getName(), user1.getName());
+        assertEquals(userTarget1.getPassword(), user1.getPassword());
 
-        assertEquals(user2.getName(), user1.getName());
-        assertEquals(user2.getPassword(), user1.getPassword());
+        User userTarget2 = userDao.get(user2.getId());
+        assertEquals(userTarget2.getName(), user2.getName());
+        assertEquals(userTarget2.getPassword(), user2.getPassword());
+
+    }
+
+    @DisplayName("get() 메소드의 예외 상황에 대한 테스트")
+    @Test
+    public void getUserFailure() throws SQLException {
+
+        ApplicationContext context = new GenericXmlApplicationContext("spring/applicationContext.xml");
+
+        final UserDao userDao = context.getBean("userDao", UserDao.class);
+        userDao.deleteAll();
+        assertEquals(userDao.getCount(), 0);
+
+        assertThrows(EmptyResultDataAccessException.class, () -> userDao.get("unknown"));
     }
 }
