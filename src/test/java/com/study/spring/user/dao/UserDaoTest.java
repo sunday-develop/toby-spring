ac91110751aa445dd1185fea65261af3bd10c5a4
최홍희 <vvshinevv@tmon.co.kr>
2020-03-14 02:32:43 +0900

    [2.4.1]import제거

--- src/test/java/com/study/spring/user/dao/UserDaoTest.java
+++ src/test/java/com/study/spring/user/dao/UserDaoTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
