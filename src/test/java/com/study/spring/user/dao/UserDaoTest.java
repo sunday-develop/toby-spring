453f298ca6196560bdc76e22fabe8f5c5c18c651
최홍희 <vvshinevv@tmon.co.kr>
2020-03-14 00:35:10 +0900

    [2.2.2]테스트의 효율적인 수행과 결과 관리

--- src/test/java/com/study/spring/user/dao/UserDaoTest.java
+++ src/test/java/com/study/spring/user/dao/UserDaoTest.java
@@ -1,20 +1,27 @@
-import com.study.spring.user.dao.UserDao;
+package com.study.spring.user.dao;
+
 import com.study.spring.user.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
-public class UserDaoTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
-    public static void main(String[] args) throws SQLException {
+class UserDaoTest {
+
+    @DisplayName("add() 메소드와 get() 메소드에 대한 테스트")
+    @Test
+    void addAndGet() throws SQLException {
 
         ApplicationContext context = new GenericXmlApplicationContext("spring/applicationContext.xml");
         UserDao userDao = context.getBean("userDao", UserDao.class);
 
         User user1 = new User();
         user1.setId("whiteship");
-        user1.setName("백기선");
+        user1.setName("back");
         user1.setPassword("married");
 
         userDao.add(user1);
@@ -23,12 +30,7 @@ public class UserDaoTest {
 
         User user2 = userDao.get(user1.getId());
 
-        if (!user1.getName().equals(user2.getName())) {
-            System.out.println("테스트 실패 (name)");
-        } else if (!user1.getPassword().equals(user2.getPassword())) {
-            System.out.println("테스트 실패 (password");
-        } else {
-            System.out.println(user2.getId() + " 조회 성공");
-        }
+        assertThat(user2.getName()).isEqualTo(user1.getName());
+        assertThat(user2.getPassword()).isEqualTo(user1.getPassword());
     }
 }
