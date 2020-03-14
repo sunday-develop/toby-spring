58521d0e547eff5f616b81f545836222b32afbb1
mouthgaebal <mouthgaebal@gmail.com>
2020-03-15 01:25:23 +0900

    리스트 2-21, 2-22

---
+++ src/test/java/springbook/config/TestConfig.java
@@ -0,0 +1,29 @@
+package springbook.config;
+
+import com.mysql.cj.jdbc.Driver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import springbook.user.dao.UserDao;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class TestConfig {
+
+    @Bean
+    public UserDao userDao() {
+        return new UserDao(dataSource());
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        final SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost:3306/springbook");
+        dataSource.setUsername("spring");
+        dataSource.setPassword("book");
+        return dataSource;
+    }
+
+}
