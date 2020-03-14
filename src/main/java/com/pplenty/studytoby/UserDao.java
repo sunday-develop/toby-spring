938c7c94ab10c27443a45d44534b5d08d298b1d8
pplenty <newtype26@naver.com>
2020-03-14 18:41:58 +0900

    2-14: 예외 테스트

--- src/main/java/com/pplenty/studytoby/UserDao.java
+++ src/main/java/com/pplenty/studytoby/UserDao.java
@@ -1,5 +1,7 @@
 package com.pplenty.studytoby;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -72,15 +74,22 @@ public class UserDao {
         ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
-        rs.next();
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
+
+        User user = null;
+        if (rs.next()) {
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
 
         rs.close();
         ps.close();
         con.close();
+
+        if (user == null) {
+            throw new EmptyResultDataAccessException(1);
+        }
 
         return user;
 
--- src/test/java/com/pplenty/studytoby/chapter02/UserDaoTest.java
+++ src/test/java/com/pplenty/studytoby/chapter02/UserDaoTest.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Created by yusik on 2020/03/14.
@@ -34,20 +36,32 @@ public class UserDaoTest {
     void addAndGet() throws SQLException {
 
         // given
-        User user = new User();
-        user.setId("koh");
-        user.setName("yusik");
-        user.setPassword("1234");
+        User user1 = new User("pplenty", "yusik", "1234");
+        User user2 = new User("kohyusik", "usik", "4321");
 
         // when
-        userDao.add(user);
-        User result = userDao.get("koh");
-        int count = userDao.getCount();
+        userDao.add(user1);
+        userDao.add(user2);
 
         // then
-        assertThat(count).isEqualTo(1);
-        assertThat(result.getId()).isEqualTo(user.getId());
-        assertThat(result.getPassword()).isEqualTo(user.getPassword());
+        assertThat(userDao.getCount()).isEqualTo(2);
+
+        User result1 = userDao.get(user1.getId());
+        assertThat(result1.getId()).isEqualTo(user1.getId());
+        assertThat(result1.getPassword()).isEqualTo(user1.getPassword());
+
+        User result2 = userDao.get(user2.getId());
+        assertThat(result2.getId()).isEqualTo(user2.getId());
+        assertThat(result2.getPassword()).isEqualTo(user2.getPassword());
+    }
+
+    @DisplayName("존재하지 않는 사용자")
+    @Test
+    void getUserFailure() throws SQLException {
+
+        // then
+        assertThat(userDao.getCount()).isEqualTo(0);
+        assertThrows(EmptyResultDataAccessException.class, () -> userDao.get("unknown_id"));
     }
 
     @DisplayName("사용자 생성 및 조회(junit5)")
