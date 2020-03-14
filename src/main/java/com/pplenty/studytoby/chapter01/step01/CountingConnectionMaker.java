e203327b96683728d146cc02c85a1cfedb09161b
pplenty <newtype26@naver.com>
2020-03-14 15:02:51 +0900

    1-30: 관심사가 다른(DB 연결 횟수 카운팅) 빈을 중간에 주입

---
+++ src/main/java/com/pplenty/studytoby/chapter01/step01/CountingConnectionMaker.java
@@ -0,0 +1,27 @@
+package com.pplenty.studytoby.chapter01.step01;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by yusik on 2020/03/14.
+ */
+public class CountingConnectionMaker implements ConnectionMaker {
+
+    int counter = 0;
+    private ConnectionMaker realConnectionMaker;
+
+    public CountingConnectionMaker(ConnectionMaker realConnectionMaker) {
+        this.realConnectionMaker = realConnectionMaker;
+    }
+
+    @Override
+    public Connection makeConnection() throws SQLException {
+        counter++;
+        return realConnectionMaker.makeConnection();
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+}
