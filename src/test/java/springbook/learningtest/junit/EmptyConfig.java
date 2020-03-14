c89ff35e7424b7048a0912389e0613625c098cc5
mouthgaebal <mouthgaebal@gmail.com>
2020-03-15 01:41:09 +0900

    리스트 2-26, 2-27

---
+++ src/test/java/springbook/learningtest/junit/EmptyConfig.java
@@ -0,0 +1,8 @@
+package springbook.learningtest.junit;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmptyConfig {
+
+}
--- src/test/java/springbook/learningtest/junit/JUnitTest.java
+++ src/test/java/springbook/learningtest/junit/JUnitTest.java
@@ -1,32 +1,52 @@
 package springbook.learningtest.junit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = EmptyConfig.class)
 public class JUnitTest {
 
+    @Autowired
+    private ApplicationContext context;
+
     private static Set<JUnitTest> testObjects = new HashSet<>();
+    private static ApplicationContext contextObject = null;
 
     @Test
     void test1() throws Exception {
         assertThat(testObjects).isNotIn(this);
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == context).isTrue();
+        contextObject = context;
     }
 
     @Test
     void test2() throws Exception {
         assertThat(testObjects).isNotIn(this);
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == context).isTrue();
+        contextObject = context;
     }
 
     @Test
     void test3() throws Exception {
         assertThat(testObjects).isNotIn(this);
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == context).isTrue();
+        contextObject = context;
     }
 
 }
