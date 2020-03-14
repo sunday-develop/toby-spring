3c103a39c1aed113015e11e615a9a121a84bd52d
mouthgaebal <mouthgaebal@gmail.com>
2020-03-15 02:17:00 +0900

    리스트 3-19

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -19,15 +19,13 @@ public class UserDao {
 
     public void add(User user) throws SQLException {
 
-        final StatementStrategy strategy = con -> {
+        jdbcContextWithStatementStrategy(con -> {
             final PreparedStatement ps = con.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
             ps.setString(1, user.getId());
             ps.setString(2, user.getName());
             ps.setString(3, user.getPassword());
             return ps;
-        };
-
-        jdbcContextWithStatementStrategy(strategy);
+        });
     }
 
     public User get(String id) throws SQLException {
