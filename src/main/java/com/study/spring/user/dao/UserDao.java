6f2923ae486b9349acfb8bae8690665091847dca
최홍희 <vvshinevv@tmon.co.kr>
2020-03-14 17:04:11 +0900

    [3.4.2]jdbcContext의 특별한 DI

--- src/main/java/com/study/spring/user/dao/UserDao.java
+++ src/main/java/com/study/spring/user/dao/UserDao.java
@@ -15,11 +15,10 @@ public class UserDao {
     private JdbcContext jdbcContext;
 
     public void setDataSource(DataSource dataSource) {
+        this.jdbcContext = new JdbcContext();
+        this.jdbcContext.setDataSource(dataSource);
         this.dataSource = dataSource;
-    }
 
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
     }
 
     public void add(final User user) throws SQLException {
