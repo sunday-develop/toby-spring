87d57134c8a59543af9121360ccfb4fec6013781
pplenty <newtype26@naver.com>
2020-03-14 16:50:27 +0900

    1-40: XML 설정에 의한 context load

--- src/test/java/com/pplenty/studytoby/StudyTobyApplicationTests.java
+++ src/test/java/com/pplenty/studytoby/StudyTobyApplicationTests.java
@@ -1,13 +1,22 @@
 package com.pplenty.studytoby;
 
+import com.pplenty.studytoby.chapter01.step01.DaoFactory;
+import com.pplenty.studytoby.chapter01.step01.UserDao;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = DaoFactory.class)
 class StudyTobyApplicationTests {
+
+    @Autowired
+    private UserDao userDao;
 
     @Test
     void contextLoads() {
+        System.out.println(userDao);
     }
 
 }
---
+++ src/test/java/com/pplenty/studytoby/chapter01/step01/UserDaoTestFromXml.java
@@ -0,0 +1,71 @@
+package com.pplenty.studytoby.chapter01.step01;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by yusik on 2020/03/14.
+ */
+public class UserDaoTestFromXml {
+
+    @BeforeEach
+    void setUp() throws SQLException {
+
+        Connection con = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:63306/toby", "jason", "qwe123");
+
+        PreparedStatement ps = con.prepareStatement("truncate toby.users");
+        ps.executeUpdate();
+
+    }
+
+    @DisplayName("빈 조회 테스트 - GenericXmlApplicationContext")
+    @Test
+    void genericXml() {
+
+        // given
+        ApplicationContext context = new GenericXmlApplicationContext("/di/userDaoFactory.xml");
+
+        // when
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+        UserDao userDao2 = context.getBean("userDao", UserDao.class);
+
+        // then
+        assertThat(userDao == userDao2).isTrue();
+        assertThat(userDao).isEqualTo(userDao2);
+        System.out.println(userDao);
+        System.out.println(userDao2);
+
+    }
+
+    @DisplayName("빈 조회 테스트 - ClassPathXmlApplicationContext")
+    @Test
+    void classPathXml() {
+
+        // given
+//        ApplicationContext context = new ClassPathXmlApplicationContext("userDao.xml", UserDao.class);
+        ApplicationContext context = new ClassPathXmlApplicationContext("/di/userDaoFactory.xml");
+
+        // when
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+        UserDao userDao2 = context.getBean("userDao", UserDao.class);
+
+        // then
+        assertThat(userDao == userDao2).isTrue();
+        assertThat(userDao).isEqualTo(userDao2);
+        System.out.println(userDao);
+        System.out.println(userDao2);
+
+    }
+}
