5d137e66cb0a1872c2df11dae5cc620571a48042
최홍희 <vvshinevv@tmon.co.kr>
2020-03-14 14:33:45 +0900

    [2.5.2]테스트 케이스 추가

--- src/test/java/com/study/spring/junit/JUnitTest.java
+++ src/test/java/com/study/spring/junit/JUnitTest.java
@@ -2,11 +2,17 @@ package com.study.spring.junit;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 public class JUnitTest {
 
     static JUnitTest testObject;
+    static Set<JUnitTest> testObjects = new HashSet<>();
 
     @Test
     public void test1() {
@@ -26,4 +32,21 @@ public class JUnitTest {
         testObject = this;
     }
 
+    @Test
+    public void test4() {
+        assertFalse(testObjects.contains(testObject));
+        testObjects.add(this);
+    }
+
+    @Test
+    public void test5() {
+        assertFalse(testObjects.contains(testObject));
+        testObjects.add(this);
+    }
+
+    @Test
+    public void test6() {
+        assertFalse(testObjects.contains(testObject));
+        testObjects.add(this);
+    }
 }
