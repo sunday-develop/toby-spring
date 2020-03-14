7bcdefbbbd4b4ec07af08f3b0b1738b54ab9ad92
최홍희 <vvshinevv@tmon.co.kr>
2020-03-14 15:43:48 +0900

    [3.2.2.-4]DI를 위한 클라이언트 / 컨텍스트 분리

--- src/main/java/com/study/spring/user/dao/UserDao.java
+++ src/main/java/com/study/spring/user/dao/UserDao.java
@@ -59,34 +59,8 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-            StatementStrategy strategy = new DeleteAllStrategy();
-            ps = strategy.makePreparedStatement(c);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException ignored) {
-
-                }
-            }
-
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException ignored) {
-
-                }
-            }
-        }
+        StatementStrategy st = new DeleteAllStrategy();
+        jdbcContextWithStatementStrategy(st);
     }
 
     public int getCount() throws SQLException {
@@ -132,5 +106,35 @@ public class UserDao {
                 }
             }
         }
+    }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy statementStrategy) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+            ps = statementStrategy.makePreparedStatement(c);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ignored) {
+
+                }
+            }
+
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException ignored) {
+
+                }
+            }
+        }
+
     }
 }
