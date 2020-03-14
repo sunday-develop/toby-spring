c879cdbc473b039dd17d0e7fc156143c5931a6be
김다혜 <dahye432111@gmail.com>
2020-03-14 14:10:00 +0900

    2장: 학습테스트 - junit
    JUnit 실행 시 오브젝트가 매번 새로 생기는 지 확인하는 학습테스트
    - 테스트 실행 순서에 상관없이 매번 새로운 오브젝트가 생기는지 확인하도록 refactoring

--- src/test/java/com/toby/tobyspring/learningtest/junit/JUnitTest.java
+++ src/test/java/com/toby/tobyspring/learningtest/junit/JUnitTest.java
@@ -1,26 +1,31 @@
 package com.toby.tobyspring.learningtest.junit;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@DisplayName("junit은 매번 새로운 오브젝트를 생성할까 ?")
 public class JUnitTest {
-    static JUnitTest testObject;
+    static Set<JUnitTest> testObjects = new HashSet<>();
 
     @Test
     public void test1() {
-        Assertions.assertNotSame(this, testObject);
-        testObject = this;
+        Assertions.assertFalse(testObjects.contains(this));
+        testObjects.add(this);
     }
 
     @Test
     public void test2() {
-        Assertions.assertNotSame(this, testObject);
-        testObject = this;
+        Assertions.assertFalse(testObjects.contains(this));
+        testObjects.add(this);
     }
 
     @Test
     public void test3() {
-        Assertions.assertNotSame(this, testObject);
-        testObject = this;
+        Assertions.assertFalse(testObjects.contains(this));
+        testObjects.add(this);
     }
 }
