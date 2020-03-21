b00228f9fb6e6edfe185ee2f5311108b63e6d5be
pplenty <newtype26@naver.com>
2020-03-22 03:22:17 +0900

    4-24: 예외 전환 테스트

--- src/test/java/com/pplenty/studytoby/chapter04/ExceptionTest.java
+++ src/test/java/com/pplenty/studytoby/chapter04/ExceptionTest.java
@@ -2,15 +2,20 @@ package com.pplenty.studytoby.chapter04;
 
 import com.pplenty.studytoby.User;
 import com.pplenty.studytoby.UserDao;
-import com.pplenty.studytoby.UserDaoJdbc;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.test.context.ContextConfiguration;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
@@ -20,6 +25,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @SpringBootTest
 @ContextConfiguration("classpath:test-applicationContext.xml")
 public class ExceptionTest {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Autowired
     private UserDao userDao;
@@ -36,6 +44,27 @@ public class ExceptionTest {
             userDao.add(new User("1", "name", "123"));
             userDao.add(new User("1", "name", "123"));
         });
+
+    }
+
+    @DisplayName("스프링 예외 전환")
+    @Test
+    void duplicateUserException() {
+
+        try {
+
+            userDao.add(new User("1", "name", "123"));
+            userDao.add(new User("1", "name", "123"));
+
+        } catch (DuplicateKeyException ex) {
+            ex.printStackTrace();
+            SQLException sqlEx = (SQLException) ex.getRootCause();
+            SQLExceptionTranslator translator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
+
+            assertThat(translator.translate(null, null, sqlEx))
+                    .isInstanceOf(DuplicateKeyException.class);
+        }
+
 
     }
 
