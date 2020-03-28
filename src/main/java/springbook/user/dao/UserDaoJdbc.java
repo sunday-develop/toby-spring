d360f6c61ef3939b3b7c89c3e8264e43d44fd0ed
mouthgaebal <mouthgaebal@gmail.com>
2020-03-28 19:54:53 +0900

    리스트 5-9

--- src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -1,11 +1,13 @@
 package springbook.user.dao;
 
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
 import java.util.List;
+import java.util.Map;
 
 public class UserDaoJdbc implements UserDao {
 
@@ -13,38 +15,55 @@ public class UserDaoJdbc implements UserDao {
             .id(rs.getString("id"))
             .name(rs.getString("name"))
             .password(rs.getString("password"))
+            .level(Level.valueOf(rs.getInt("level")))
+            .login(rs.getInt("login"))
+            .recommend(rs.getInt("recommend"))
             .build();
 
-    private final JdbcTemplate jdbcTemplate;
+    private static final String USER_ADD = "insert into users(id, name, password, level, login, recommend) " +
+            "values(:id, :name, :password, :level, :login, :recommend)";
+
+    private static final String USER_GET = "select * from users where id = :id";
+    private static final String USER_DELETE_ALL = "delete from users";
+    private static final String USER_GET_COUNT = "select count(*) from users";
+    private static final String USER_GET_ALL = "select * from users order by id";
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
     public UserDaoJdbc(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
     }
 
     @Override
     public void add(User user) {
-        jdbcTemplate.update("insert into users(id, name, password) values(?, ?, ?)",
-                user.getId(), user.getName(), user.getPassword());
+        jdbcTemplate.update(USER_ADD, Map.<String, Object>of(
+                "id", user.getId(),
+                "name", user.getName(),
+                "password", user.getPassword(),
+                "level", user.getLevel().intValue(),
+                "login", user.getLogin(),
+                "recommend", user.getRecommend()
+        ));
     }
 
     @Override
     public User get(String id) {
-        return jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id}, USER_MAPPER);
+        return jdbcTemplate.queryForObject(USER_GET, Map.<String, Object>of("id", id), USER_MAPPER);
     }
 
     @Override
     public void deleteAll() {
-        jdbcTemplate.update("delete from users");
+        jdbcTemplate.update(USER_DELETE_ALL, Map.of());
     }
 
     @Override
     public int getCount() {
-        return jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+        return jdbcTemplate.queryForObject(USER_GET_COUNT, Map.of(), Integer.class);
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("select * from users order by id", USER_MAPPER);
+        return jdbcTemplate.query(USER_GET_ALL, USER_MAPPER);
     }
 
 }
