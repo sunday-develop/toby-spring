2d07e3732db9d085367cac2cd9f8a9895c7571bb
mouthgaebal <mouthgaebal@gmail.com>
2020-03-28 21:06:45 +0900

    리스트 5-24, 5-25

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -21,10 +21,26 @@ public class UserService {
     }
 
     private boolean canUpgradeLevel(User user) {
-        return false;
+        final Level currentLevel = user.getLevel();
+        switch (currentLevel) {
+            case BASIC:
+                return user.getLogin() >= 50;
+            case SILVER:
+                return user.getRecommend() >= 30;
+            case GOLD:
+                return false;
+            default:
+                throw new IllegalArgumentException("Unknown Level : " + currentLevel);
+        }
     }
 
     private void upgradeLevel(User user) {
+        if (user.getLevel() == Level.BASIC) {
+            user.setLevel(Level.SILVER);
+        } else if (user.getLevel() == Level.SILVER) {
+            user.setLevel(Level.GOLD);
+        }
+        userDao.update(user);
     }
 
     public void add(User user) {
