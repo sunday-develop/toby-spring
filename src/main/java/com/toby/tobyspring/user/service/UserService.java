57aa8f42459c368780c62473462abdd3f391490d
김다혜 <dahye432111@gmail.com>
2020-03-28 16:34:44 +0900

    5장: 사용자 레벨 관리 기능 추가 - 레벨 관리 기능 구현
    - 사용자 관리 로직을 위해 UserService 생성 (비지니스 로직을 담을 클래스)
    - UserService는 UserDao를 사용할 것이기 때문에 빈으로 등록되어야 한다.

---
+++ src/main/java/com/toby/tobyspring/user/service/UserService.java
@@ -0,0 +1,38 @@
+package com.toby.tobyspring.user.service;
+
+import com.toby.tobyspring.user.dao.UserDao;
+import com.toby.tobyspring.user.domain.Grade;
+import com.toby.tobyspring.user.domain.User;
+
+import java.util.List;
+
+public class UserService {
+    UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void upgradeGrades() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            Boolean changed = null;
+            if (Grade.BASIC.equals(user.getGrade()) && user.getLogin() >= 50) {
+                user.setGrade(Grade.SILVER);
+                changed = true;
+            } else if (Grade.SILVER.equals(user.getGrade()) && user.getRecomend() >= 30) {
+                user.setGrade(Grade.GOLD);
+                changed = true;
+            } else if (Grade.GOLD.equals(user.getGrade())) {
+                changed = false;
+            } else {
+                changed = false;
+            }
+
+            if (changed) {
+                userDao.update(user);
+            }
+        }
+    }
+
+}
---
+++ src/test/java/com/toby/tobyspring/user/service/UserServiceTest.java
@@ -0,0 +1,68 @@
+package com.toby.tobyspring.user.service;
+
+import com.toby.tobyspring.user.dao.UserDao;
+import com.toby.tobyspring.user.domain.Grade;
+import com.toby.tobyspring.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
+@DisplayName("userService test")
+class UserServiceTest {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserDao userDao;
+
+    List<User> users;
+
+    @BeforeEach
+    public void setUp() {
+        users = Arrays.asList(
+                new User("dahye", "김다혜", "p1", Grade.BASIC, 49, 0),
+                new User("toby", "토비", "p2", Grade.BASIC, 50, 0),
+                new User("white", "흰", "p3", Grade.SILVER, 60, 29),
+                new User("black", "검", "p4", Grade.SILVER, 60, 30),
+                new User("yellow", "노랑", "p5", Grade.GOLD, 100, 100)
+        );
+    }
+
+    @Test
+    @DisplayName("bean이 null이 아닌가 ? null이 아니라면, 빈 생성 정상적으로 된 것")
+    public void bean() {
+        assertNotNull(this.userService);
+    }
+
+    @Test
+    @DisplayName("사용자 레벨 업그레이드 테스트")
+    public void upgradeGrades() {
+        userDao.deleteAll();
+        for (User user : users) userDao.add(user);
+
+        userService.upgradeGrades();
+
+        checkGrade(users.get(0), Grade.BASIC);
+        checkGrade(users.get(1), Grade.SILVER);
+        checkGrade(users.get(2), Grade.SILVER);
+        checkGrade(users.get(3), Grade.GOLD);
+        checkGrade(users.get(4), Grade.GOLD);
+    }
+
+    private void checkGrade(User user, Grade expectedGrade) {
+        User userUpdate = userDao.get(user.getId());
+        assertEquals(expectedGrade, userUpdate.getGrade());
+    }
+}
