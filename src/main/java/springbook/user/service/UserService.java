f4297dc563055f35bdf8d7f6fde3560bb758a151
mouthgaebal <mouthgaebal@gmail.com>
2020-03-28 20:39:44 +0900

    리스트 5-18

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -1,6 +1,10 @@
 package springbook.user.service;
 
 import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+import java.util.List;
 
 public class UserService {
 
@@ -8,6 +12,24 @@ public class UserService {
 
     public UserService(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public void upgradeLevels() {
+        final List<User> users = userDao.getAll();
+        for (User user : users) {
+            boolean changed = false;
+            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+                user.setLevel(Level.SILVER);
+                changed = true;
+            } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+                user.setLevel(Level.GOLD);
+                changed = true;
+            }
+
+            if (changed) {
+                userDao.update(user);
+            }
+        }
     }
 
 }
