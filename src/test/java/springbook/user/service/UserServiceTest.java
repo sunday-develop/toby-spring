873e8a9f8efc4bb7b7f2a627c2004c4266b59af1
mouthgaebal <mouthgaebal@gmail.com>
2020-03-28 20:43:34 +0900

    리스트 5-19

--- src/test/java/springbook/user/service/UserServiceTest.java
+++ src/test/java/springbook/user/service/UserServiceTest.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import springbook.config.TestConfig;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,6 +19,14 @@ class UserServiceTest {
 
     @Autowired
     private UserService userService;
+
+    private final List<User> users = List.of(
+            User.of("bumjin", "박범진", "p1", Level.BASIC, 49, 0),
+            User.of("joytouch", "강명성", "p2", Level.BASIC, 50, 0),
+            User.of("erwins", "신승한", "p3", Level.SILVER, 60, 29),
+            User.of("madnite1", "이상호", "p4", Level.SILVER, 60, 30),
+            User.of("green", "오민규", "p5", Level.GOLD, 100, 100)
+    );
 
     @Test
     void bean() throws Exception {
