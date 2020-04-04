904d685c4ae98e611aadc612d4c6c905e9cfd7f3
hongjongwan <serverwizard@naver.com>
2020-04-04 23:51:22 +0900

    [chap5(5-4)] User에 필드 추가 (Level, login 수, recommend 수)

---
+++ src/main/java/chap5/User.java
@@ -0,0 +1,59 @@
+package chap5;
+
+public class User {
+    private String id;
+    private String name;
+    private String password;
+
+    private Level level;
+    private int login;
+    private int recommend;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public int getLogin() {
+        return login;
+    }
+
+    public void setLogin(int login) {
+        this.login = login;
+    }
+
+    public int getRecommend() {
+        return recommend;
+    }
+
+    public void setRecommend(int recommend) {
+        this.recommend = recommend;
+    }
+}
