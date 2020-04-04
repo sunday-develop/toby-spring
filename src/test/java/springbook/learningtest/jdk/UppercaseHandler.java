defc17e86b9294e4d5ef2738fd9f57959067188e
mouthgaebal <mouthgaebal@gmail.com>
2020-04-05 02:39:23 +0900

    리스트 6-26

--- src/test/java/springbook/learningtest/jdk/UppercaseHandler.java
+++ src/test/java/springbook/learningtest/jdk/UppercaseHandler.java
@@ -14,7 +14,7 @@ public class UppercaseHandler implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         final Object ret = method.invoke(target, args);
-        if (ret instanceof String) {
+        if (ret instanceof String && method.getName().startsWith("say")) {
             return ((String) ret).toUpperCase();
         }
 
