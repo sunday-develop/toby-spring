0fe7a98294f5ac6d64cbd35148ee02fa13c94bb4
mouthgaebal <mouthgaebal@gmail.com>
2020-04-05 02:18:47 +0900

    리스트 6-17

---
+++ src/test/java/springbook/learningtest/jdk/ReflectionTest.java
@@ -0,0 +1,26 @@
+package springbook.learningtest.jdk;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReflectionTest {
+
+    @Test
+    void invokeMethod() throws Exception {
+        final String name = "Spring";
+
+        assertThat(name.length()).isEqualTo(6);
+
+        final Method lengthMethod = String.class.getMethod("length");
+        assertThat(lengthMethod.invoke(name)).isEqualTo(6);
+
+        assertThat(name.charAt(0)).isEqualTo('S');
+
+        final Method charAtMethod = String.class.getMethod("charAt", int.class);
+        assertThat(charAtMethod.invoke(name, 0)).isEqualTo('S');
+    }
+
+}
