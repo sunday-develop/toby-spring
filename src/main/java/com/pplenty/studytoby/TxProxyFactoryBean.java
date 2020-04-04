32f51706ec998b881c6f00011dfd2910048bf5d2
pplenty <newtype26@naver.com>
2020-04-05 00:57:44 +0900

    6-36: 트랜잭션 프록시 팩토리 빈 등록

---
+++ src/main/java/com/pplenty/studytoby/TxProxyFactoryBean.java
@@ -0,0 +1,57 @@
+package com.pplenty.studytoby;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * Created by yusik on 2020/04/05.
+ */
+public class TxProxyFactoryBean implements FactoryBean<Object> {
+
+    Object target;
+    PlatformTransactionManager transactionManager;
+    String pattern;
+    Class<?> serviceInterface;
+
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public void setServiceInterface(Class<?> serviceInterface) {
+        this.serviceInterface = serviceInterface;
+    }
+
+    @Override
+    public Object getObject() {
+
+        TransactionHandler txHandler = new TransactionHandler();
+        txHandler.setTarget(target);
+        txHandler.setTransactionManager(transactionManager);
+        txHandler.setPattern(pattern);
+
+        return Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{serviceInterface},
+                txHandler);
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return serviceInterface;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
+}
