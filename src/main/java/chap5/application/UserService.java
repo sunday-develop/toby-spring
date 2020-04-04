fd54861a222635f6e7446acf214c4eea8198ad92
hongjongwan <serverwizard@naver.com>
2020-04-05 04:16:34 +0900

    [chap5(5-25)] UserService, canUpgradeLevel, upgradeLevel 메소드 추가

--- src/main/java/chap5/application/UserService.java
+++ src/main/java/chap5/application/UserService.java
@@ -6,6 +6,8 @@ import chap5.domain.UserDao;
 
 import java.util.List;
 
+import static chap5.domain.Level.*;
+
 public class UserService {
     private UserDao userDao;
 
@@ -16,28 +18,35 @@ public class UserService {
     public void upgradeLevels() {
         List<User> users = userDao.getAll();
         for (User user : users) {
-            Boolean changed = null;
-
-            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-                user.setLevel(Level.SILVER);
-                changed = true;
-            } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
-                user.setLevel(Level.GOLD);
-                changed = true;
-            } else if (user.getLevel() == Level.GOLD) {
-                changed = false;
-            } else {
-                changed = false;
-            }
-
-            if (changed) {
-                userDao.update(user);
+            if (canUpgradeLevel(user)) {
+                upgradeLevel(user);
             }
         }
     }
 
+    private boolean canUpgradeLevel(User user) {
+        Level userLevel = user.getLevel();
+
+        switch (userLevel) {
+            case BASIC:
+                return user.getLogin() >= 50;
+            case SILVER:
+                return user.getRecommend() >= 30;
+            case GOLD:
+                return false;
+            default:
+                throw new IllegalArgumentException("Unknown Level: " + userLevel);
+        }
+    }
+
+    private void upgradeLevel(User user) {
+        if (user.getLevel() == BASIC) user.setLevel(SILVER);
+        else if (user.getLevel() == SILVER) user.setLevel(GOLD);
+        userDao.update(user);
+    }
+
     public void add(User user) {
-        if (user.getLevel() == null) user.setLevel(Level.BASIC);
+        if (user.getLevel() == null) user.setLevel(BASIC);
         userDao.add(user);
     }
 }
