1d16231d890cbb3b59adb7f17abfcf028a546fb7
mouthgaebal <mouthgaebal@gmail.com>
2020-04-05 03:26:09 +0900

    리스트 6-41

---
+++ src/test/java/springbook/learningtest/jdk/DynamicProxyTest.java
@@ -0,0 +1,44 @@
+package springbook.learningtest.jdk;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DynamicProxyTest {
+
+    @Test
+    void simpleProxy() throws Exception {
+        final Hello hello = new HelloTarget();
+
+        assertThat(hello.sayHello("Toby")).isEqualTo("Hello Toby");
+        assertThat(hello.sayHi("Toby")).isEqualTo("Hi Toby");
+        assertThat(hello.sayThankYou("Toby")).isEqualTo("Thank You Toby");
+    }
+
+    @Test
+    void proxyFactoryBean() throws Exception {
+        final ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+        pfBean.addAdvice(new UppercaseAdvice());
+
+        final Hello proxiedHello = (Hello) pfBean.getObject();
+
+        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    private static class UppercaseAdvice implements MethodInterceptor {
+
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            final String ret = (String) invocation.proceed();
+            return ret.toUpperCase();
+        }
+
+    }
+
+}
