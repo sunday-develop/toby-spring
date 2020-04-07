78c3d14d1c34f9e9f17ef3aaf7738084baadea34
pplenty <newtype26@naver.com>
2020-04-08 00:31:04 +0900

    feat: 랭킹 적용

---
+++ src/test/java/com/pplenty/studytoby/chapter06/test/Rank.java
@@ -0,0 +1,12 @@
+package com.pplenty.studytoby.chapter06.test;
+
+/**
+ * Created by yusik on 2020/04/07.
+ */
+public enum Rank {
+    BRONZE,
+    SILVER,
+    GOLD,
+    PLATINUM,
+    DIAMOND,
+}
