14e268ec60079da6862ed89f46885b6328d8fab8
김다혜 <dahye432111@gmail.com>
2020-04-08 22:21:29 +0900

    6장: 스프링 aop - 포인트컷 표현식

---
+++ src/main/java/com/toby/tobyspring/learningtest/spring/pointcut/TargetInterface.java
@@ -0,0 +1,8 @@
+package com.toby.tobyspring.learningtest.spring.pointcut;
+
+public interface TargetInterface {
+    public void hello();
+    public void hello(String a);
+    public int minus(int a, int b) throws RuntimeException;
+    public int plus(int a, int b);
+}
