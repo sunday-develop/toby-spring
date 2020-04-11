5a34792dfd00bb2fcfb058f66dece848ad4bc990
mouthgaebal <mouthgaebal@gmail.com>
2020-04-12 00:55:11 +0900

    리스트 6-97

--- src/test/java/springbook/user/service/UserServiceTest.java
+++ src/test/java/springbook/user/service/UserServiceTest.java
@@ -11,9 +11,7 @@ import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
 import springbook.config.TestConfig;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
@@ -33,17 +31,13 @@ import static springbook.user.service.UserServiceImpl.MIN_RECOMMEND_FOR_GOLD;
 @ContextConfiguration(classes = TestConfig.class)
 public class UserServiceTest {
 
+
     @Autowired
     private UserService userService;
-
     @Autowired
     private UserService testUserService;
-
     @Autowired
     private UserDao userDao;
-
-    @Autowired
-    private PlatformTransactionManager transactionManager;
 
     private final List<User> users = List.of(
             User.of("bumjin", "박범진", "p1", Level.BASIC, MIN_LOG_COUNT_FOR_SILVER - 1, 0, "email1@email.com"),
@@ -166,17 +160,11 @@ public class UserServiceTest {
     }
 
     @Test
+    @Transactional
     void transactionSync() throws Exception {
-        final DefaultTransactionDefinition txDefinition = new DefaultTransactionDefinition();
-        final TransactionStatus txStatus = transactionManager.getTransaction(txDefinition);
-
-        try {
-            userService.deleteAll();
-            userService.add(users.get(0));
-            userService.add(users.get(1));
-        } finally {
-            transactionManager.rollback(txStatus);
-        }
+        userService.deleteAll();
+        userService.add(users.get(0));
+        userService.add(users.get(1));
     }
 
     /////////////////////////////////////////////
@@ -206,7 +194,9 @@ public class UserServiceTest {
 
     }
 
-    private static class TestUserServiceException extends RuntimeException { }
+    private static class TestUserServiceException extends RuntimeException {
+
+    }
 
     private static class MockMailSender implements MailSender {
 
@@ -222,7 +212,8 @@ public class UserServiceTest {
         }
 
         @Override
-        public void send(SimpleMailMessage... simpleMessages) throws MailException { }
+        public void send(SimpleMailMessage... simpleMessages) throws MailException {
+        }
 
     }
 
