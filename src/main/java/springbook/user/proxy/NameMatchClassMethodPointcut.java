6bb912f753b21e21b90f56df07f70622d1d26722
mouthgaebal <mouthgaebal@gmail.com>
2020-04-11 22:23:04 +0900

    리스트 6-51

---
+++ src/main/java/springbook/user/proxy/NameMatchClassMethodPointcut.java
@@ -0,0 +1,28 @@
+package springbook.user.proxy;
+
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.util.PatternMatchUtils;
+
+public class NameMatchClassMethodPointcut extends NameMatchMethodPointcut {
+
+    public void setMappedClassName(String mappedClassName) {
+        setClassFilter(new SimpleClassFilter(mappedClassName));
+    }
+
+    private static class SimpleClassFilter implements ClassFilter {
+
+        private final String mappedName;
+
+        private SimpleClassFilter(String mappedName) {
+            this.mappedName = mappedName;
+        }
+
+        @Override
+        public boolean matches(Class<?> clazz) {
+            return PatternMatchUtils.simpleMatch(mappedName, clazz.getSimpleName());
+        }
+
+    }
+
+}
