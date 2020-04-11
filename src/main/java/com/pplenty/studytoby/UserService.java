a1abe2165055b47cd14f02bef80ca25491b3ec0a
pplenty <newtype26@naver.com>
2020-04-12 03:29:27 +0900

    6-87: 선언적 트랜잭션 적용

--- src/main/java/com/pplenty/studytoby/UserService.java
+++ src/main/java/com/pplenty/studytoby/UserService.java
@@ -1,15 +1,26 @@
 package com.pplenty.studytoby;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 /**
  * Created by yusik on 2020/04/04.
  */
+@Transactional
 public interface UserService {
+
     void add(User user);
+
+    @Transactional(readOnly = true)
     User get(String id);
+
+    @Transactional(readOnly = true)
     List<User> getAll();
+
     void deleteAll();
+
     void update(User user);
+
     void upgradeLevels();
 }
