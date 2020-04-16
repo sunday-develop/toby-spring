017396899b671ef95b6ebe3697cfa1727fd6a2db
김다혜 <dahye432111@gmail.com>
2020-04-16 23:07:17 +0900

    7장: 인터페이스의 분리와 자기참조 빈 - XML 파일을 이용하는 SQL 서비스

---
+++ src/main/java/com/toby/tobyspring/user/sqlservice/XmlSqlService.java
@@ -0,0 +1,38 @@
+package com.toby.tobyspring.user.sqlservice;
+
+import com.toby.tobyspring.user.sqlservice.jaxb.SqlType;
+import com.toby.tobyspring.user.sqlservice.jaxb.Sqlmap;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class XmlSqlService implements SqlService {
+    private Map<String, String> sqlMap = new HashMap<String, String>();
+
+    public XmlSqlService() {
+        String contextPath = Sqlmap.class.getPackage().getName();
+        try {
+            JAXBContext context = JAXBContext.newInstance(contextPath);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            InputStream is = getClass().getResourceAsStream("/sql.xml");
+            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
+
+            for (SqlType sql : sqlmap.getSql()) {
+                sqlMap.put(sql.getKey(), sql.getValue());
+            }
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getSql(String key) throws SqlRetrievalFailureException {
+        String sql = this.sqlMap.get(key);
+        if (sql == null) throw new SqlRetrievalFailureException(key + "에 대한 SQL을 찾을 수 없습니다.");
+        else return sql;
+    }
+}
