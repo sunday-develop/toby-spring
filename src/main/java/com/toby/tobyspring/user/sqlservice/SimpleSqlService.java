3463c42ccb78c5dacdc10ac9e28b9ea69446c4a7
김다혜 <dahye432111@gmail.com>
2020-04-16 22:08:18 +0900

    7장: SQL과 DAO의 분리 - sql 제공 서비스

---
+++ src/main/java/com/toby/tobyspring/user/sqlservice/SimpleSqlService.java
@@ -0,0 +1,18 @@
+package com.toby.tobyspring.user.sqlservice;
+
+import java.util.Map;
+
+public class SimpleSqlService implements SqlService {
+    private Map<String, String> sqlMap;
+
+    public void setSqlMap(Map<String, String> sqlMap) {
+        this.sqlMap = sqlMap;
+    }
+
+    @Override
+    public String getSql(String key) throws SqlRetrievalFailureException {
+        String sql = this.sqlMap.get(key);
+        if (sql == null) throw new SqlRetrievalFailureException(key + "에 대한 SQL을 찾을 수 없습니다.");
+        else return sql;
+    }
+}
