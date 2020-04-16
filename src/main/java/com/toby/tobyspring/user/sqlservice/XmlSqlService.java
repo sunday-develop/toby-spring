1d9667485eafaa452c9e8e1619eae0528106634a
김다혜 <dahye432111@gmail.com>
2020-04-16 23:18:53 +0900

    7장: 인터페이스의 분리와 자기참조 빈 - 빈의 초기화 작업

--- src/main/java/com/toby/tobyspring/user/sqlservice/XmlSqlService.java
+++ src/main/java/com/toby/tobyspring/user/sqlservice/XmlSqlService.java
@@ -3,6 +3,7 @@ package com.toby.tobyspring.user.sqlservice;
 import com.toby.tobyspring.user.sqlservice.jaxb.SqlType;
 import com.toby.tobyspring.user.sqlservice.jaxb.Sqlmap;
 
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -11,14 +12,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class XmlSqlService implements SqlService {
+    private String sqlmapFile;
     private Map<String, String> sqlMap = new HashMap<String, String>();
 
-    public XmlSqlService() {
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
+    }
+
+    @PostConstruct
+    public void loadSql() {
         String contextPath = Sqlmap.class.getPackage().getName();
         try {
             JAXBContext context = JAXBContext.newInstance(contextPath);
             Unmarshaller unmarshaller = context.createUnmarshaller();
-            InputStream is = getClass().getResourceAsStream("/sql.xml");
+            InputStream is = getClass().getResourceAsStream(this.sqlmapFile);
             Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 
             for (SqlType sql : sqlmap.getSql()) {
