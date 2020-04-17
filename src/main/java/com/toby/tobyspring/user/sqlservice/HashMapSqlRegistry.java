e1ed7b877a6e6896b0495906ac6a2ad3bec51c64
김다혜 <dahye432111@gmail.com>
2020-04-17 22:09:54 +0900

    7장: 인터페이스의 분리와 자기참조 빈 - 디폴트 의존관계

---
+++ src/main/java/com/toby/tobyspring/user/sqlservice/HashMapSqlRegistry.java
@@ -0,0 +1,20 @@
+package com.toby.tobyspring.user.sqlservice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapSqlRegistry implements SqlRegistry {
+    private Map<String, String> sqlMap = new HashMap<String, String>();
+
+    @Override
+    public void registerSql(String key, String sql) {
+        sqlMap.put(key, sql);
+    }
+
+    @Override
+    public String findSql(String key) throws SqlNotFoundException {
+        String sql = this.sqlMap.get(key);
+        if (sql == null) throw new SqlRetrievalFailureException(key + "에 대한 SQL을 찾을 수 없습니다.");
+        else return sql;
+    }
+}
---
+++ src/main/java/com/toby/tobyspring/user/sqlservice/JaxbXmlReader.java
@@ -0,0 +1,34 @@
+package com.toby.tobyspring.user.sqlservice;
+
+import com.toby.tobyspring.user.sqlservice.jaxb.SqlType;
+import com.toby.tobyspring.user.sqlservice.jaxb.Sqlmap;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+
+public class JaxbXmlReader implements SqlReader {
+    private String sqlmapFile;
+
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
+    }
+
+    @Override
+    public void read(SqlRegistry sqlRegistry) {
+        String contextPath = Sqlmap.class.getPackage().getName();
+        try {
+            JAXBContext context = JAXBContext.newInstance(contextPath);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            InputStream is = getClass().getResourceAsStream(this.sqlmapFile);
+            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
+
+            for (SqlType sql : sqlmap.getSql()) {
+                sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+            }
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
