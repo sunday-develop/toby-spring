2cef60b1a4ad367d452a1bb6f01a4ad16cde480f
김다혜 <dahye432111@gmail.com>
2020-04-23 00:15:12 +0900

    7장: 서비스 추상화 적용 - 위임을 이용한 BaseSqlService의 재사용

--- src/main/java/com/toby/tobyspring/user/sqlservice/OxmSqlService.java
+++ src/main/java/com/toby/tobyspring/user/sqlservice/OxmSqlService.java
@@ -10,6 +10,7 @@ import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 
 public class OxmSqlService implements SqlService {
+    private final BaseSqlService baseSqlService = new BaseSqlService();
     private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
     private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
 
@@ -27,16 +28,14 @@ public class OxmSqlService implements SqlService {
 
     @PostConstruct
     public void loadSql() {
-        this.oxmSqlReader.read(this.sqlRegistry);
+        this.baseSqlService.setSqlReader(this.oxmSqlReader);
+        this.baseSqlService.setSqlRegistry(this.sqlRegistry);
+        this.baseSqlService.loadSql();
     }
 
     @Override
     public String getSql(String key) throws SqlRetrievalFailureException {
-        try {
-            return this.sqlRegistry.findSql(key);
-        } catch (SqlNotFoundException e) {
-            throw new SqlRetrievalFailureException(e);
-        }
+        return this.baseSqlService.getSql(key);
     }
 
     private class OxmSqlReader implements SqlReader {
