9db570f07129f3721fd33fd009b4cb93c03b0f0b
김다혜 <dahye432111@gmail.com>
2020-04-22 23:49:08 +0900

    7장: 서비스 추상화 적용 - OXM 서비스 추상화 적용

---
+++ src/main/java/com/toby/tobyspring/user/sqlservice/OxmSqlService.java
@@ -0,0 +1,69 @@
+package com.toby.tobyspring.user.sqlservice;
+
+import com.toby.tobyspring.user.sqlservice.jaxb.SqlType;
+import com.toby.tobyspring.user.sqlservice.jaxb.Sqlmap;
+import org.springframework.oxm.Unmarshaller;
+
+import javax.annotation.PostConstruct;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+
+public class OxmSqlService implements SqlService {
+    private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
+    private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
+
+    public void setSqlRegistry(SqlRegistry sqlRegistry) {
+        this.sqlRegistry = sqlRegistry;
+    }
+
+    public void setUnmarshaller(Unmarshaller unmarshaller) {
+        this.oxmSqlReader.setUnmarshaller(unmarshaller);
+    }
+
+    public void setSqlmapFile(String sqlmapFile) {
+        this.oxmSqlReader.setSqlmapFile(sqlmapFile);
+    }
+
+    @PostConstruct
+    public void loadSql() {
+        this.oxmSqlReader.read(this.sqlRegistry);
+    }
+
+    @Override
+    public String getSql(String key) throws SqlRetrievalFailureException {
+        try {
+            return this.sqlRegistry.findSql(key);
+        } catch (SqlNotFoundException e) {
+            throw new SqlRetrievalFailureException(e);
+        }
+    }
+
+    private class OxmSqlReader implements SqlReader {
+        private Unmarshaller unmarshaller;
+        private static final String DEFAULT_SQLMAP_FILE = "/sql.xml";
+        private String sqlmapFile = DEFAULT_SQLMAP_FILE;
+
+        public void setUnmarshaller(Unmarshaller unmarshaller) {
+            this.unmarshaller = unmarshaller;
+        }
+
+        public void setSqlmapFile(String sqlmapFile) {
+            this.sqlmapFile = sqlmapFile;
+        }
+
+        @Override
+        public void read(SqlRegistry sqlRegistry) {
+            try {
+                Source source = new StreamSource(getClass().getResourceAsStream(this.sqlmapFile));
+                Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(source);
+
+                for (SqlType sql : sqlmap.getSql()) {
+                    sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+                }
+            } catch (IOException e) {
+                throw new IllegalArgumentException(this.sqlmapFile + "을 가져올 수 없습니다.", e);
+            }
+        }
+    }
+}
