762aa49406bd0718c1f8974d48ba55d35909a34c
김다혜 <dahye432111@gmail.com>
2020-04-24 23:16:12 +0900

    7장: 내장형 데이터베이스를 이용한 SQL 레지스트리 만들기

--- src/main/java/com/toby/tobyspring/issuetracker/exception/SqlUpdateFailureException.java
+++ src/main/java/com/toby/tobyspring/issuetracker/exception/SqlUpdateFailureException.java
@@ -1,6 +1,10 @@
 package com.toby.tobyspring.issuetracker.exception;
 
 public class SqlUpdateFailureException extends RuntimeException {
+    public SqlUpdateFailureException(String msg) {
+        super(msg);
+    }
+
     public SqlUpdateFailureException(Throwable cause) {
         super(cause);
     }
