0ab473f72543ba6a13690d018390ecf6bd353a74
mouthgaebal <mouthgaebal@gmail.com>
2020-04-26 01:49:51 +0900

    리스트 7-74, 7-75

--- src/test/java/springbook/user/sqlservice/ConcurrentHashMapSqlRegistryTest.java
+++ src/test/java/springbook/user/sqlservice/ConcurrentHashMapSqlRegistryTest.java
@@ -1,63 +1,10 @@
 package springbook.user.sqlservice;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+class ConcurrentHashMapSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
 
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-class ConcurrentHashMapSqlRegistryTest {
-
-    private UpdatableSqlRegistry sqlRegistry;
-
-    @BeforeEach
-    void setup() {
-        sqlRegistry = new ConcurrentHashMapSqlRegistry();
-        sqlRegistry.registerSql("KEY1", "SQL1");
-        sqlRegistry.registerSql("KEY2", "SQL2");
-        sqlRegistry.registerSql("KEY3", "SQL3");
-    }
-
-    @Test
-    void find() throws Exception {
-        checkFindResult("SQL1", "SQL2", "SQL3");
-    }
-
-    private void checkFindResult(String expected1, String expected2, String expected3) {
-        assertThat(sqlRegistry.findSql("KEY1")).isEqualTo(expected1);
-        assertThat(sqlRegistry.findSql("KEY2")).isEqualTo(expected2);
-        assertThat(sqlRegistry.findSql("KEY3")).isEqualTo(expected3);
-    }
-
-    @Test
-    void unknownKey() throws Exception {
-        assertThatThrownBy(() -> sqlRegistry.findSql("SQL9999!@#$"))
-            .isInstanceOf(SqlNotFoundException.class);
-    }
-
-    @Test
-    void updateSingle() throws Exception {
-        sqlRegistry.updateSql("KEY2", "Modified2");
-        checkFindResult("SQL1", "Modified2", "SQL3");
-    }
-
-    @Test
-    void updateMulti() throws Exception {
-        final Map<String, String> sqlmap = Map.of(
-                "KEY1", "Modified1",
-                "KEY3", "Modified3"
-        );
-
-        sqlRegistry.updateSql(sqlmap);
-        checkFindResult("Modified1", "SQL2", "Modified3");
-    }
-
-    @Test
-    void updateWithNotExistingKey() throws Exception {
-        assertThatThrownBy(() -> sqlRegistry.updateSql("SQL9999!@#$", "Modified2"))
-                .isInstanceOf(SqlUpdateFailureException.class);
+    @Override
+    protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
+        return new ConcurrentHashMapSqlRegistry();
     }
 
 }
