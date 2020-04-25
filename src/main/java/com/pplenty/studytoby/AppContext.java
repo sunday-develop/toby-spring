b6f5fab8b0db458012c1aa4730aee6aae40a18dd
pplenty <newtype26@naver.com>
2020-04-26 02:52:01 +0900

    7-134: SqlMapConfig 을 Config 에서 직접 구현

--- src/main/java/com/pplenty/studytoby/AppContext.java
+++ src/main/java/com/pplenty/studytoby/AppContext.java
@@ -7,6 +7,8 @@ import org.mariadb.jdbc.Driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
@@ -24,7 +26,7 @@ import javax.sql.DataSource;
 @ComponentScan(basePackages = "com.pplenty.studytoby")
 @EnableTransactionManagement
 @Configuration
-public class AppContext {
+public class AppContext implements SqlMapConfig {
 
     @Value("${db.driverClass}")
     Class<? extends Driver> driverClass;
@@ -67,9 +69,9 @@ public class AppContext {
         return new UserLevelUpgradeEventPolicy();
     }
 
-    @Bean
-    public SqlMapConfig sqlMapConfig() {
-        return new UserSqlMapConfig();
+    @Override
+    public Resource getSqlMapResource() {
+        return new ClassPathResource("src/test/resources/test-sqlmap.xml");
     }
 
     @Configuration
