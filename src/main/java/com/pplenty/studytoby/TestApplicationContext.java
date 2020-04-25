4559a7892cbf02fac8bd342a3c8b9880c391dea4
pplenty <newtype26@naver.com>
2020-04-26 01:34:03 +0900

    7-83: DI 메타 정보로 사용될 설정 클래스

---
+++ src/main/java/com/pplenty/studytoby/TestApplicationContext.java
@@ -0,0 +1,10 @@
+package com.pplenty.studytoby;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by yusik on 2020/04/26.
+ */
+@Configuration
+public class TestApplicationContext {
+}
