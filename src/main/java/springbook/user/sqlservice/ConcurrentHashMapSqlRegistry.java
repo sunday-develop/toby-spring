ba93be917a5a9f6cd9f0a7d1b55f5506cc5f1cc8
mouthgaebal <mouthgaebal@gmail.com>
2020-04-26 01:11:24 +0900

    리스트 7-66

--- src/main/java/springbook/user/sqlservice/ConcurrentHashMapSqlRegistry.java
+++ src/main/java/springbook/user/sqlservice/ConcurrentHashMapSqlRegistry.java
@@ -1,27 +1,40 @@
 package springbook.user.sqlservice;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ConcurrentHashMapSqlRegistry implements UpdatableSqlRegistry {
 
-    @Override
-    public void updateSql(String key, String sql) throws SqlUpdateFailureException {
-
-    }
+    private final Map<String, String> sqlMap = new ConcurrentHashMap<>();
 
     @Override
-    public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
-
+    public String findSql(String key) throws SqlNotFoundException {
+        final String sql = sqlMap.get(key);
+        if (sql == null) {
+            throw new SqlNotFoundException(key + "에 대한 SQL을 찾을 수 없습니다.");
+        }
+        return sql;
     }
 
     @Override
     public void registerSql(String key, String sql) {
-
+        sqlMap.put(key, sql);
     }
 
     @Override
-    public String findSql(String key) throws SqlNotFoundException {
-        return null;
+    public void updateSql(String key, String sql) throws SqlUpdateFailureException {
+        if (!sqlMap.containsKey(key)) {
+            throw new SqlUpdateFailureException(key + "에 해당하는 SQL 을 찾을 수 없습니다.");
+        }
+        sqlMap.put(key, sql);
+    }
+
+    @Override
+    public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
+        for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
+            updateSql(entry.getKey(), entry.getValue());
+        }
+
     }
 
 }
