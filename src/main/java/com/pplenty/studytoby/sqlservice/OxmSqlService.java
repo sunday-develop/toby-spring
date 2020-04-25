7da7ab04933ffbd0f569defc19cdd1a4ba203779
pplenty <newtype26@naver.com>
2020-04-25 21:35:25 +0900

    7-52: OxmSqlService 완성

--- src/main/java/com/pplenty/studytoby/sqlservice/OxmSqlService.java
+++ src/main/java/com/pplenty/studytoby/sqlservice/OxmSqlService.java
@@ -1,6 +1,16 @@
 package com.pplenty.studytoby.sqlservice;
 
+import com.pplenty.studytoby.sqlservice.jaxb.SqlType;
+import com.pplenty.studytoby.sqlservice.jaxb.Sqlmap;
 import org.springframework.oxm.Unmarshaller;
+
+import javax.annotation.PostConstruct;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
 
 /**
  * Created by yusik on 2020/04/25.
@@ -9,27 +19,59 @@ public class OxmSqlService implements SqlService {
 
     private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
 
+    private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
+
+    @PostConstruct
+    public void loadSql() {
+        oxmSqlReader.read(this.sqlRegistry);
+    }
+
     @Override
     public String getSql(String key) throws SqlRetrievalFailureException {
-        return null;
+        try {
+            return sqlRegistry.findSql(key);
+        } catch (SqlNotFoundException e) {
+            throw new SqlRetrievalFailureException(e);
+        }
+    }
+
+    public void setSqlRegistry(SqlRegistry sqlRegistry) {
+        this.sqlRegistry = sqlRegistry;
     }
 
     public void setUnmarshaller(Unmarshaller unmarshaller) {
         this.oxmSqlReader.setUnmarshaller(unmarshaller);
     }
 
-    public void setSqlmapFile(String sqlmapFile) {
-        this.oxmSqlReader.setSqlmapFile(sqlmapFile);
+    public void setSqlMapFile(String sqlMapFile) {
+        this.oxmSqlReader.setSqlMapFile(sqlMapFile);
     }
 
 
     private static class OxmSqlReader implements SqlReader {
 
+        private static final String DEFAULT_SQLMAP_FILE = "src/main/resources/di/user-sqlmap.xml";
+
+        private String sqlMapFile = DEFAULT_SQLMAP_FILE;
         private Unmarshaller unmarshaller;
-        private String sqlmapFile;
 
         @Override
         public void read(SqlRegistry sqlRegistry) {
+            try {
+                String contextPath = Sqlmap.class.getPackage().getName();
+                JAXBContext context;
+                context = JAXBContext.newInstance(contextPath);
+
+                javax.xml.bind.Unmarshaller unmarshaller = context.createUnmarshaller();
+                Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(new FileInputStream(new File(this.sqlMapFile)));
+
+                List<SqlType> sqlList = sqlmap.getSql();
+                for (SqlType sqlType : sqlList) {
+                    sqlRegistry.registerSql(sqlType.getKey(), sqlType.getValue());
+                }
+            } catch (JAXBException | FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
 
         }
 
@@ -37,8 +79,8 @@ public class OxmSqlService implements SqlService {
             this.unmarshaller = unmarshaller;
         }
 
-        public void setSqlmapFile(String sqlmapFile) {
-            this.sqlmapFile = sqlmapFile;
+        public void setSqlMapFile(String sqlMapFile) {
+            this.sqlMapFile = sqlMapFile;
         }
     }
 }
