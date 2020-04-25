8f7777f16752946d5a4fea0e236754095fc426a8
pplenty <newtype26@naver.com>
2020-04-26 01:45:25 +0900

    7-88: Java Config - dataSource

--- src/test/java/com/pplenty/studytoby/TestApplicationContext.java
+++ src/test/java/com/pplenty/studytoby/TestApplicationContext.java
@@ -1,7 +1,12 @@
 package com.pplenty.studytoby;
 
+import org.mariadb.jdbc.Driver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * Created by yusik on 2020/04/26.
@@ -9,5 +14,16 @@ import org.springframework.context.annotation.ImportResource;
 @Configuration
 @ImportResource("classpath:test-applicationContext.xml")
 public class TestApplicationContext {
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(Driver.class);
+        dataSource.setUrl("jdbc:mariadb://localhost:63306/toby");
+        dataSource.setUsername("jason");
+        dataSource.setPassword("qwe123");
+
+        return dataSource;
+    }
 }
 
