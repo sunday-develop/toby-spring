e2429409a10aa404ed40d48b5a96da403268a0b5
pplenty <newtype26@naver.com>
2020-04-26 02:37:54 +0900

    7-120: 중첩 클래스로 합치기

--- src/main/java/com/pplenty/studytoby/AppContext.java
+++ src/main/java/com/pplenty/studytoby/AppContext.java
@@ -1,15 +1,13 @@
 package com.pplenty.studytoby;
 
-import com.pplenty.studytoby.sqlservice.ProductionAppContext;
 import com.pplenty.studytoby.sqlservice.SqlServiceContext;
 import org.mariadb.jdbc.Driver;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -18,7 +16,7 @@ import javax.sql.DataSource;
 /**
  * Created by yusik on 2020/04/26.
  */
-@Import({SqlServiceContext.class, ProductionAppContext.class})
+@Import({SqlServiceContext.class, AppContext.ProductionAppContext.class})
 @ComponentScan(basePackages = "com.pplenty.studytoby")
 @EnableTransactionManagement
 @Configuration
@@ -50,6 +48,17 @@ public class AppContext {
     @Bean
     public UserLevelUpgradePolicy policy() {
         return new UserLevelUpgradeEventPolicy();
+    }
+
+    @Configuration
+    @Profile("prod")
+    public static class ProductionAppContext {
+        @Bean
+        public MailSender mailSender() {
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            mailSender.setHost("alt1.gmail-smtp-in.l.google.com");
+            return mailSender;
+        }
     }
 
 }
--- src/test/java/com/pplenty/studytoby/TestAppContext.java
+++ src/test/java/com/pplenty/studytoby/TestAppContext.java
@@ -4,18 +4,14 @@ import com.pplenty.studytoby.chapter05.TestUserService;
 import com.pplenty.studytoby.chapter05.UserServiceTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.mail.MailSender;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Created by yusik on 2020/04/26.
  */
 @Profile("test")
-@ComponentScan(basePackages = "com.pplenty.studytoby")
-@EnableTransactionManagement
 @Configuration
 public class TestAppContext {
 
