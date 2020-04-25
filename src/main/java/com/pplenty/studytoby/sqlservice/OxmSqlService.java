e95d7f9202bb1174d6e40ad93dff5ed88fd3a8be
pplenty <newtype26@naver.com>
2020-04-25 21:31:03 +0900

    7-51: 내부 오브젝트로 위임

--- src/main/java/com/pplenty/studytoby/sqlservice/OxmSqlService.java
+++ src/main/java/com/pplenty/studytoby/sqlservice/OxmSqlService.java
@@ -1,5 +1,7 @@
 package com.pplenty.studytoby.sqlservice;
 
+import org.springframework.oxm.Unmarshaller;
+
 /**
  * Created by yusik on 2020/04/25.
  */
@@ -12,10 +14,31 @@ public class OxmSqlService implements SqlService {
         return null;
     }
 
-    private class OxmSqlReader implements SqlReader {
+    public void setUnmarshaller(Unmarshaller unmarshaller) {
+        this.oxmSqlReader.setUnmarshaller(unmarshaller);
+    }
+
+    public void setSqlmapFile(String sqlmapFile) {
+        this.oxmSqlReader.setSqlmapFile(sqlmapFile);
+    }
+
+
+    private static class OxmSqlReader implements SqlReader {
+
+        private Unmarshaller unmarshaller;
+        private String sqlmapFile;
+
         @Override
         public void read(SqlRegistry sqlRegistry) {
 
+        }
+
+        public void setUnmarshaller(Unmarshaller unmarshaller) {
+            this.unmarshaller = unmarshaller;
+        }
+
+        public void setSqlmapFile(String sqlmapFile) {
+            this.sqlmapFile = sqlmapFile;
         }
     }
 }
