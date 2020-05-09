f9c780b57dab1cfce9b3e0cb511d9148ece5543c
김다혜 <dahye432111@gmail.com>
2020-05-09 14:23:08 +0900

    7장: 자바 코드를 이용한 빈 설정

---
+++ src/test/java/com/toby/tobyspring/config/TestApplicationContext.java
@@ -0,0 +1,9 @@
+package com.toby.tobyspring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("/applicationContext.xml")
+public class TestApplicationContext {
+}
