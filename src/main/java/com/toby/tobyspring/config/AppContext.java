0a51588032570e44da89809e0e61335296d65b58
김다혜 <dahye432111@gmail.com>
2020-05-09 16:00:27 +0900

    7장: 컨텍스트 분리와 @Import - @Import

--- src/main/java/com/toby/tobyspring/config/AppContext.java
+++ src/main/java/com/toby/tobyspring/config/AppContext.java
@@ -1,32 +1,23 @@
 package com.toby.tobyspring.config;
 
-import com.toby.tobyspring.issuetracker.sqlservice.EmbeddedDbSqlRegistry;
 import com.toby.tobyspring.user.dao.UserDao;
-import com.toby.tobyspring.user.service.*;
-import com.toby.tobyspring.user.sqlservice.OxmSqlService;
-import com.toby.tobyspring.user.sqlservice.SqlRegistry;
-import com.toby.tobyspring.user.sqlservice.SqlService;
 import oracle.jdbc.driver.OracleDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.mail.MailSender;
-import org.springframework.oxm.Unmarshaller;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
-import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
-
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "com.toby.tobyspring")
+@Import(SqlServiceContext.class)
 public class AppContext {
 
     @Autowired
@@ -49,37 +40,6 @@ public class AppContext {
         DataSourceTransactionManager tm = new DataSourceTransactionManager();
         tm.setDataSource(dataSource());
         return tm;
-    }
-
-    @Bean
-    public SqlService sqlService() {
-        OxmSqlService sqlService = new OxmSqlService();
-        sqlService.setUnmarshaller(unmarshaller());
-        sqlService.setSqlRegistry(sqlRegistry());
-        return sqlService;
-    }
-
-    @Bean
-    public DataSource embeddedDatabase() {
-        return new EmbeddedDatabaseBuilder()
-                .setName("embeddedDatabase")
-                .setType(HSQL)
-                .addScript("classpath:sql/schema.sql")
-                .build();
-    }
-
-    @Bean
-    public SqlRegistry sqlRegistry() {
-        EmbeddedDbSqlRegistry sqlRegistry = new EmbeddedDbSqlRegistry();
-        sqlRegistry.setDataSource(embeddedDatabase());
-        return sqlRegistry;
-    }
-
-    @Bean
-    public Unmarshaller unmarshaller() {
-        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-        marshaller.setContextPath("com.toby.tobyspring.user.sqlservice.jaxb");
-        return marshaller;
     }
 
 }
